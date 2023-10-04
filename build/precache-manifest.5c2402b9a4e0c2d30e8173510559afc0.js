d7d5190e77fcd6032a90423b4bde91f5372e38f9
Shields003 <emailshields1@gmail.com>
2023-10-04 11:05:32 -0500

    Put new build file in backend

---
+++ build/precache-manifest.5c2402b9a4e0c2d30e8173510559afc0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "43da782497d2748e5cdd",
+    "url": "/static/css/main.c172f05b.chunk.css"
+  },
+  {
+    "revision": "43da782497d2748e5cdd",
+    "url": "/static/js/main.4e04eea2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d4bded9be43532a0ece7",
+    "url": "/static/css/2.03f8264b.chunk.css"
+  },
+  {
+    "revision": "d4bded9be43532a0ece7",
+    "url": "/static/js/2.348de376.chunk.js"
+  },
+  {
+    "revision": "c8374d0801d1c0bffb5a97203f30dcef",
+    "url": "/static/media/pulseLogoSmall.c8374d08.png"
+  },
+  {
+    "revision": "86bf6388cff94f6e37ecdfc08a1da34b",
+    "url": "/static/media/bockscarLogo.86bf6388.png"
+  },
+  {
+    "revision": "5d48897e2f30fab80b3402146d9a9215",
+    "url": "/static/media/pulseLogoLarge.5d48897e.jpeg"
+  },
+  {
+    "revision": "94701482cfd6801f1f8cb10289f47592",
+    "url": "/index.html"
+  }
+];
